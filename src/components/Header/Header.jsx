f70457ffba25ee4f15bc1d3f60125fe3f1a52d50
samsur4D <patarehan14600@gmail.com>
2024-03-15 14:18:23 +0600

    MAtro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
         <header className='flex justify-between items-center p-4 border-b-2 max-w-7xl mx-auto'>
-            <h1 className='text-4xl font-bold'>Knowladge Cafe !!!</h1>
+            <h1 className='text-4xl font-bold'>Knowladge Cafe </h1>
             <img src={profile} alt="" />
         </header>
     );
